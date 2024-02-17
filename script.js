611fc07b5d704541f78af7e6702b959b0cd19b84
Akash Kundu <aakassh.uit@gmail.com>
2024-02-17 22:05:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+ function checkloveper(){
+
+  var name = document.getElementById('name').value;
+  var lname = document.getElementById('lname').value;
+
+  if(name == ""){
+   alert('Please enter your name');
+  }else if (name.length <=2 ) {
+   alert('Min lenght is 3')
+  }else if(!isNaN(name)){
+   alert('Numbers are not allowed');
+  }
+
+  else if(lname == ""){
+   alert('Please enter your love name');
+  }else if (lname.length <=2 ) {
+   alert('Min lenght is 3')
+  }else if(!isNaN(lname)){
+   alert('Numbers are not allowed');
+  }
+  else{
+  var lovdata = Math.random() * 100;
+  lovdata = Math.floor(lovdata);
+  document.getElementById('lovevalue').value = lovdata + "%"; 
+  }
+  var message = "";
+    if (lovePercentage >= 80) {
+      message = "Wow! You are meant to be together!";
+    } else if (lovePercentage >= 50) {
+      message = "Looks like there's potential!";
+    } else {
+      message = "Hmm... Keep working on it!";
+    }
+    
+    document.getElementById('message').innerText = message;
+  
+ }
